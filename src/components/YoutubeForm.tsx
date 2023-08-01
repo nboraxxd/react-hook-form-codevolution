5aa11cd00a505809793fafd15eb90e5aa447d9fd
bora <nhatbaoxxd@gmail.com>
2023-08-01 19:31:06 +0700

    register, handleSubmit of useForm and Devtool

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,18 +1,37 @@
+import { useForm } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
+
+interface IYouTubeForm {
+  username: string
+  email: string
+  channel: string
+}
+
 export default function YoutubeForm() {
+  const { register, control, handleSubmit } = useForm<IYouTubeForm>()
+
+  const onSubmit = (data: IYouTubeForm) => {
+    console.log(data)
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input type="text" id="username" {...register('username')} />
 
         <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
+        <input type="email" id="email" {...register('email')} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" id="channel" />
+        <input type="text" id="channel" {...register('channel')} />
 
-        <button className="submit-btn" type="submit">Submit</button>
+        <button className="submit-btn" type="submit">
+          Submit
+        </button>
       </form>
+
+      <DevTool control={control} />
     </div>
   )
 }
