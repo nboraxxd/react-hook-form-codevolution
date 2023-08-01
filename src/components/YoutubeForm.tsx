65004d8ccae86951258f92055b3eeae3646fb92c
bora <nhatbaoxxd@gmail.com>
2023-08-01 19:57:03 +0700

    Handle error in React Hook Form

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -8,7 +8,12 @@ interface IYouTubeForm {
 }
 
 export default function YoutubeForm() {
-  const { register, control, handleSubmit } = useForm<IYouTubeForm>()
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IYouTubeForm>()
 
   const onSubmit = (data: IYouTubeForm) => {
     console.log(data)
@@ -18,13 +23,34 @@ export default function YoutubeForm() {
     <div>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register('username')} />
+        <input type="text" id="username" {...register('username', { required: 'Username is required' })} />
+        <p className="error">{errors.username?.message}</p>
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register('email')} />
+        <input
+          type="email"
+          id="email"
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /^\S+@\S+\.\S+$/,
+              message: 'Invalid email format',
+            },
+            minLength: {
+              value: 5,
+              message: 'Email must be at least 5 characters in length',
+            },
+            maxLength: {
+              value: 160,
+              message: 'Emails are only allowed to have a maximum of 160 characters',
+            },
+          })}
+        />
+        <p className="error">{errors.email?.message}</p>
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register('channel')} />
+        <input type="text" id="channel" {...register('channel', { required: 'Channel is required' })} />
+        <p className="error">{errors.channel?.message}</p>
 
         <button className="submit-btn" type="submit">
           Submit
